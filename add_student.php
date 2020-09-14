dfc1516d1e2ae704dc270e001591c868818bc313
exclusiveTanim <exclusiveman420@gmail.com>
2020-09-14 09:54:08 +0600

    Update on 14th Sep

---
+++ add_student.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="parent">
+<h2>HTML Forms</h2>
+
+<form action="action_page.php" method="post">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" ><br>
+  <label for="fname">Email:</label><br>
+  <input type="text" id="email" name="email"><br>
+  <label for="lname">Mobile:</label><br>
+  <input type="text" id="mobile" name="mobile" ><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "action_page.php".</p>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,22 +1,39 @@
 <!DOCTYPE html>
 <html>
-<body>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<style>
+		.parent{
+			height: 500px;
+	        width: 400px;
+	        margin: 0 auto;
+	        padding: 20px;
+	        border: 1px solid red;
+	        }
+	</style>
+</head>
+<body style="background-image:linear-gradient(to bottom, #9966ff 23%, #99ccff 100%)">
 
-<h2>HTML Forms</h2>
+<h2 style="text-align: center;
+    color: white;
+    font-family: Impact, Charcoal, sans-serif;
+    margin-top: 20px;">Login Forms</h2>
+<div class="parent">
+  <img src="prof.jpg" class="rounded-circle" alt="Cinque Terre">
+  <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <button type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>
+</form>
 
-<form action="action_page.php" method="post">
-  <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname"><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" ><br>
-  <label for="fname">Email:</label><br>
-  <input type="text" id="email" name="email"><br>
-  <label for="lname">Mobile:</label><br>
-  <input type="text" id="mobile" name="mobile" ><br><br>
-  <input type="submit" value="Submit">
-</form> 
-
-<p>If you click the "Submit" button, the form-data will be sent to a page called "action_page.php".</p>
-
+</div>
 </body>
 </html>
